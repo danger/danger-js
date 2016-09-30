0eca18d938967894d29a50883e269319672e10f3
Orta Therox <orta.therox@gmail.com>
2016-09-30 11:02:35 +0100

    Start the project

--- source/danger.js
+++ source/danger.js
@@ -1,9 +1,22 @@
 // @flow
-//
 // This file represents the module that is exposed as the danger API
 
-module.exports = function (percent: number = 100, amount: number): number {
-  const percentOff = (percent / 100) * amount
-  return percentOff
+type DangerGit = {
+  modified_files: string[],
+  created_files: string[],
+  deleted_files: string[]
 }
 
+type DangerDSL = {
+  git: DangerGit
+}
+
+const danger: DangerDSL = {
+  git: {
+    modified_files: ["hello world"],
+    created_files: ["other file"],
+    deleted_files: ["last file"]
+  }
+}
+
+export default danger
