77e8978bb99444d7c69c5d2ea099058656aeb136
OJ Kwon <kwon.ohjoong@gmail.com>
2017-01-04 13:45:48 -0800

    test(runner): update test cases for runner

--- source/runner/_tests/fixtures/ExampleDangerResults.ts
+++ source/runner/_tests/fixtures/ExampleDangerResults.ts
@@ -1,24 +1,22 @@
-// @flow
-
-import type { DangerResults } from "../../runner/DangerResults"
+import { DangerResults } from '../../../dsl/DangerResults';
 
 export const emptyResults: DangerResults = {
   fails: [],
   warnings: [],
   messages: [],
   markdowns: []
-}
+};
 
 export const warnResults: DangerResults = {
   fails: [],
-  warnings: [{ message: "Warning message" }],
+  warnings: [{ message: 'Warning message' }],
   messages: [],
   markdowns: []
-}
+};
 
 export const failsResults: DangerResults = {
-  fails: [{ message: "Failing message" }],
+  fails: [{ message: 'Failing message' }],
   warnings: [],
   messages: [],
   markdowns: []
-}
+};
