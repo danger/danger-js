155f466c6af14b2f5dab7a6ef6f071246346f885
OJ Kwon <kwon.ohjoong@gmail.com>
2017-01-04 13:47:37 -0800

    style(commands): update type definition for commands

--- source/commands/danger-local.js
+++
@@ -1,6 +0,0 @@
-// @flow
-
-var program = require("commander")
-
-program
-  .parse(process.argv)
---
+++ source/commands/danger-pr.ts
@@ -0,0 +1,5 @@
+import * as program from 'commander';
+
+program.parse(process.argv);
+
+console.log('Not Yet implmented');
