51da45febfd87f30c68d5d7c33325e8e59235865
OJ Kwon <kwon.ohjoong@gmail.com>
2017-01-04 13:41:17 -0800

    feat(ambient): update ambient type definition

---
+++ source/ambient.d.ts
@@ -0,0 +1,8 @@
+declare module 'node-fetch';
+declare module 'parse-diff';
+declare module 'lodash.includes';
+declare module 'lodash.find';
+declare module 'jest-runtime';
+declare module 'jest-environment-node';
+
+declare module '*/package.json';
