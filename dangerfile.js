57a50161d4e50fd0f0ca28f01a06bf26c8e9fb41
Macklin Underdown <macklinu@gmail.com>
2017-01-05 11:22:23 -0500

    Ignore changelog entry request if PR declared #trivial

--- dangerfile.js
+++ dangerfile.js
@@ -1,15 +1,16 @@
 // import { danger, warn } from "danger"
 import fs from "fs"
 
-// Request a CHANGELOG entry
+// Request a CHANGELOG entry if not declared #trivial
 const hasChangelog = danger.git.modified_files.includes("changelog.md")
-if (!hasChangelog) { warn("Please add a changelog entry for your changes.") }
+const isTrivial = (danger.github.pr.body + danger.github.pr.title).includes("#trivial")
+if (!hasChangelog && !isTrivial) {
+  warn("Please add a changelog entry for your changes.")
 
-// Politely ask for their name on the entry too
-const changelogDiff = danger.git.diffForFile("changelog.md")
-const contributorName = danger.github.pr.user.login
-if (changelogDiff && changelogDiff.indexOf(contributorName) === -1) {
-  warn("Please add your GitHub name to the changelog entry, so we can attribute you.")
+  // Politely ask for their name on the entry too
+  const changelogDiff = danger.git.diffForFile("changelog.md")
+  const contributorName = danger.github.pr.user.login
+  if (changelogDiff && changelogDiff.indexOf(contributorName) === -1) {
+    warn("Please add your GitHub name to the changelog entry, so we can attribute you.")
+  }
 }
-
-const jsFiles = danger.git.created_files.filter(path => path.endsWith("js"))
