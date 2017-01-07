3a4bae00fe2085d63fa7f6f667c0023838ecb1fb
OJ Kwon <kwon.ohjoong@gmail.com>
2017-01-06 23:21:07 -0800

    feat(debug): debug module for dump status

--- source/commands/danger.ts
+++ source/commands/danger.ts
@@ -3,9 +3,13 @@
 // import app from "./app"
 import { version } from "../../package.json"
 import * as program from "commander"
+import * as debug from "debug"
+
+const d = debug("danger:runner")
+
+d(`argv: ${process.argv}`)
 
 // Provides the root node to the command-line architecture
-
 program
   .version(version)
   .command("run", "Runs danger on your local system", {isDefault: true})
