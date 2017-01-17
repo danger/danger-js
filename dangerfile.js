c06c20c26bdc5d5f3c4a3c31bef41cf91861b5c7
Macklin Underdown <macklinu@gmail.com>
2017-01-17 15:44:22 -0500

    Warn if changes to package.json and not yarn.lock
    Based on https://github.com/facebook/jest/pull/2508
    
    This would have applied to https://github.com/danger/danger-js/pull/101

--- dangerfile.js
+++ dangerfile.js
@@ -1,5 +1,6 @@
 // import { danger, warn } from "danger"
 import fs from "fs"
+import includes from "lodash.includes"
 
 // Request a CHANGELOG entry if not declared #trivial
 const hasChangelog = danger.git.modified_files.includes("changelog.md")
@@ -13,4 +14,13 @@ if (!hasChangelog && !isTrivial) {
   if (changelogDiff && changelogDiff.indexOf(contributorName) === -1) {
     warn("Please add your GitHub name to the changelog entry, so we can attribute you.")
   }
+}
+
+// Warns if there are changes to package.json without changes to yarn.lock.
+const packageChanged = includes(danger.git.modified_files, "package.json")
+const lockfileChanged = includes(danger.git.modified_files, "yarn.lock")
+if (packageChanged && !lockfileChanged) {
+  const message = "Changes were made to package.json, but not to yarn.lock"
+  const idea = "Perhaps you need to run `yarn install`?"
+  warn(`${message} - <i>${idea}</i>`)
 }
