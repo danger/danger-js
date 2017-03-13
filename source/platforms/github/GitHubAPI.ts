e810980030b8e6333b09c9fbc47c21539d6bbfc5
Matt Ashbourne <matt.ashbourne@gmail.com>
2017-03-13 15:15:25 -0400

    use consistent naming
    DANGER_GITHUB_API_BASE_URL like ruby version

--- source/platforms/github/GitHubAPI.ts
+++ source/platforms/github/GitHubAPI.ts
@@ -130,7 +130,7 @@ export class GitHubAPI {
       headers["Authorization"] = `token ${this.token}`
     }
     
-    const baseUrl = process.env["DANGER_GITHUB_API_HOST"] || "https://api.github.com"
+    const baseUrl = process.env["DANGER_GITHUB_API_BASE_URL"] || "https://api.github.com"
     
     return this.fetch(`${baseUrl}/${path}`, {
       method: method,
