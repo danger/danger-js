1b739b582affd17d778139d048da8f58d6b8eb72
Orta Therox <orta.therox@gmail.com>
2017-03-16 18:03:51 +0900

    Update lockfile

--- source/ambient.d.ts
+++ source/ambient.d.ts
@@ -3,6 +3,7 @@ declare module "lodash.includes";
 declare module "lodash.find";
 declare module "jest-runtime";
 declare module "jest-environment-node";
+declare module "jest-config";
 declare module "voca";
 declare module "jsome";
 
