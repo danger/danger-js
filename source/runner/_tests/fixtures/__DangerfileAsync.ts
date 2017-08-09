e2e8a7db5cccd0d9702ef6423c416d20c7d8a226
Orta Therox <orta.therox@gmail.com>
2017-08-09 08:14:48 -0400

    More work on vm2 support

---
+++ source/runner/_tests/fixtures/__DangerfileAsync.ts
@@ -0,0 +1,14 @@
+/* tslint-disable */
+
+const asyncAction = () =>
+  new Promise(res => {
+    setTimeout(() => {
+      warn("Async Function")
+      res()
+    }, 50)
+  })
+
+schedule(async () => {
+  await asyncAction()
+  warn("After Async Function")
+})
