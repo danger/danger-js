277d6bd2d6bd67b4c83295276790aa2f7c445a2d
Orta Therox <orta.therox@gmail.com>
2017-08-28 08:26:43 -0400

    Adds a check for relative files with typescript

--- dangerfile.ts
+++ dangerfile.ts
@@ -48,23 +48,23 @@ schedule(yarn())
 // This also serves as the "one true DSL" for a Danger run against a PR
 // which tools can then work against.
 
-// import dtsGenerator from "./scripts/danger-dts"
-// const currentDTS = dtsGenerator()
-// const savedDTS = fs.readFileSync("source/danger.d.ts").toString()
-// if (currentDTS !== savedDTS) {
-//   const message = "There are changes to the Danger DSL which are not reflected in the current danger.d.ts."
-//   const idea = "Please run <code>yarn declarations</code> and update this PR."
-//   fail(`${message}<br/><i>${idea}</i>`)
-// }
+import dtsGenerator from "./scripts/danger-dts"
+const currentDTS = dtsGenerator()
+const savedDTS = fs.readFileSync("source/danger.d.ts").toString()
+if (currentDTS !== savedDTS) {
+  const message = "There are changes to the Danger DSL which are not reflected in the current danger.d.ts."
+  const idea = "Please run <code>yarn declarations</code> and update this PR."
+  fail(`${message}<br/><i>${idea}</i>`)
+}
 
 // Always ensure we name all CI providers in the README. These
 // regularly get forgotten on a PR adding a new one.
 
-// import { realProviders } from "./source/ci_source/providers"
-// import Fake from "./source/ci_source/providers/Fake"
-// const readme = fs.readFileSync("README.md").toString()
-// const names = realProviders.map(p => new p({}).name)
-// const missing = names.filter(n => !readme.includes(n))
-// if (missing.length) {
-//   warn(`These providers are missing from the README: ${sentence(missing)}`)
-// }
+import { realProviders } from "./source/ci_source/providers"
+import Fake from "./source/ci_source/providers/Fake"
+const readme = fs.readFileSync("README.md").toString()
+const names = realProviders.map(p => new p({}).name)
+const missing = names.filter(n => !readme.includes(n))
+if (missing.length) {
+  warn(`These providers are missing from the README: ${sentence(missing)}`)
+}
---
+++ source/runner/_tests/fixtures/export/other.ts
@@ -0,0 +1,5 @@
+/* tslint-disable */
+
+export default function thing() {
+  return "thing"
+}
