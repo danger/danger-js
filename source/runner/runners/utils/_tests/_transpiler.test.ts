46cc98c4840c746bccd9b268188593c8e7f650f9
Orta Therox <orta.therox@gmail.com>
2017-12-14 14:41:38 -0500

    Adds an overwrite for the TS module system when transpiling our TS code

---
+++ source/runner/runners/utils/_tests/_transpiler.test.ts
@@ -0,0 +1,22 @@
+jest.mock("fs", () => ({
+  readFileSync: jest.fn(),
+}))
+
+import { typescriptify } from "../transpiler"
+import * as fs from "fs"
+
+describe("typescriptify", () => {
+  it("removes the module option in a tsconfig ", () => {
+    const dangerfile = `import {a} from 'lodash'; a()`
+    const fakeTSConfig = {
+      compilerOptions: {
+        target: "es5",
+        module: "es2015",
+      },
+    }
+    const fsMock = fs.readFileSync as any
+    fsMock.mockImplementationOnce(() => JSON.stringify(fakeTSConfig))
+
+    expect(typescriptify(dangerfile)).not.toContain("import")
+  })
+})
