6ad8fe9d1d855fe769106702430c35ac78979c0b
Orta Therox <orta.therox@gmail.com>
2018-01-06 19:36:55 -0500

    Get git details

--- source/platforms/git/localGetDiff.ts
+++ source/platforms/git/localGetDiff.ts
@@ -1,1 +1,20 @@
-export const localGetDiff = () => ""
+import * as debug from "debug"
+import { exec } from "child_process"
+
+const d = debug("danger:localGetDiff")
+
+export const localGetDiff = (base: string, head: string) =>
+  new Promise(done => {
+    const call = `git diff --no-index ${base} ${head}`
+    d(call)
+
+    exec(call, (err, stdout, _stderr) => {
+      if (err) {
+        console.error(`Could not get diff from git between ${base} and ${head}`)
+        console.error(err)
+        return
+      }
+
+      done(stdout)
+    })
+  })
--- source/platforms/git/localGetFileAtSHA.ts
+++ source/platforms/git/localGetFileAtSHA.ts
@@ -1,1 +1,20 @@
-export const localGetFileAtSHA = () => ""
+import * as debug from "debug"
+import { exec } from "child_process"
+
+const d = debug("danger:localGetFileAtSHA")
+
+export const localGetFileAtSHA = (path: string, _repo: string | undefined, sha: string) =>
+  new Promise(done => {
+    const call = `git show ${sha}:"${path}"`
+    d(call)
+
+    exec(call, (err, stdout, _stderr) => {
+      if (err) {
+        console.error(`Could not get the file ${path} from git at ${sha}`)
+        console.error(err)
+        return
+      }
+
+      done(stdout)
+    })
+  })
