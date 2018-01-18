be8bdf07366ad0ed795f618ecbae9770182a02bc
tychota <tychota@users.noreply.github.com>
2018-01-18 14:05:04 +0100

    Add bitrise as a CI provider

---
+++ source/ci_source/providers/_tests/_bitrise.test.ts
@@ -0,0 +1,75 @@
+import { Bitrise } from "../Bitrise"
+import { getCISourceForEnv } from "../../get_ci_source"
+
+const correctEnv = {
+  BITRISE_IO: "true",
+  BITRISE_PULL_REQUEST: "800",
+  GIT_REPOSITORY_URL: "https://github.com/artsy/eigen",
+}
+
+describe("being found when looking for CI", () => {
+  it("finds Bitrise with the right ENV", () => {
+    const ci = getCISourceForEnv(correctEnv)
+    expect(ci).toBeInstanceOf(Bitrise)
+  })
+})
+
+describe(".isCI", () => {
+  it("validates when all Bitrise environment vars are set", () => {
+    const bitrise = new Bitrise(correctEnv)
+    expect(bitrise.isCI).toBeTruthy()
+  })
+
+  it("does not validate without env", () => {
+    const bitrise = new Bitrise({})
+    expect(bitrise.isCI).toBeFalsy()
+  })
+})
+
+describe(".isPR", () => {
+  it("validates when all bitrise environment vars are set", () => {
+    const bitrise = new Bitrise(correctEnv)
+    expect(bitrise.isPR).toBeTruthy()
+  })
+
+  it("does not validate outside of bitrise", () => {
+    const bitrise = new Bitrise({})
+    expect(bitrise.isPR).toBeFalsy()
+  })
+
+  const envs = ["BITRISE_PULL_REQUEST", "GIT_REPOSITORY_URL", "BITRISE_IO"]
+  envs.forEach((key: string) => {
+    let env = { ...correctEnv }
+    env[key] = null
+
+    it(`does not validate when ${key} is missing`, () => {
+      const bitrise = new Bitrise(env)
+      expect(bitrise.isCI && bitrise.isPR).toBeFalsy()
+    })
+  })
+})
+
+describe(".pullRequestID", () => {
+  it("pulls it out of the env", () => {
+    const bitrise = new Bitrise({
+      BITRISE_PULL_REQUEST: "800",
+    })
+    expect(bitrise.pullRequestID).toEqual("800")
+  })
+})
+
+describe(".repoSlug", () => {
+  it("derives it from the repo URL", () => {
+    const bitrise = new Bitrise(correctEnv)
+    expect(bitrise.repoSlug).toEqual("artsy/eigen")
+  })
+
+  it("derives it from the repo URL in SSH format", () => {
+    const env = {
+      ...correctEnv,
+      GIT_REPOSITORY_URL: "git@github.com:artsy/eigen.git",
+    }
+    const bitrise = new Bitrise(env)
+    expect(bitrise.repoSlug).toEqual("artsy/eigen")
+  })
+})
