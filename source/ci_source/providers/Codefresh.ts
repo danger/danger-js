daacef48b62b5ea2e1a8f9977adb40b35e61ffa8
Steven Peterson <steven.peterson@gmail.com>
2019-04-19 13:56:00 -0700

    Update Codefresh.ts
    - Required PR number to be an integer
    - Remove unnecessary `repoURL()` implementation
    - Other cleanup
    - Add documenation for CI setup

--- source/ci_source/providers/Codefresh.ts
+++ source/ci_source/providers/Codefresh.ts
@@ -1,7 +1,35 @@
 import { Env, CISource } from "../ci_source"
-import { ensureEnvKeysExist } from "../ci_source_helpers"
+import { ensureEnvKeysExist, ensureEnvKeysAreInt } from "../ci_source_helpers"
 
-/// Codefresh environment variables: https://codefresh.io/docs/docs/codefresh-yaml/variables/
+/**
+ *  ### CI Setup
+ *
+ *  To set up Danger on Codefresh, create a freestyle step in your Codefresh yaml configuration:
+ *
+ *  ```yml
+ *  Danger:
+ *    title: Run Danger
+ *    image: node:latest
+ *    working_directory: ${{main_clone}}
+ *    entry_point: '/bin/bash'
+ *    cmd:
+ *      - '-ce'
+ *      - |
+ *        npm install -g yarn
+ *        yarn add danger --dev
+ *        yarn danger ci --failOnErrors
+ *    when:
+ *      steps:
+ *        - name: main_clone
+ *          on:
+ *            - success
+ *  ```
+ *
+ *  The `failOnErrors` option is required in order to ensure that the step fails properly when Danger fails. If you don't wnat this behavior, you can remove this option.
+ *
+ *  Don't forget to add the `DANGER_GITHUB_API_TOKEN` variable to your pipeline settings so that Danger can properly post comments to your pull request.
+ *
+ */
 
 export class Codefresh implements CISource {
   constructor(private readonly env: Env) {}
@@ -11,11 +39,14 @@ export class Codefresh implements CISource {
   }
 
   get isCI(): boolean {
-    return ensureEnvKeysExist(this.env, ["CF_BUILD_ID"])
+    return ensureEnvKeysExist(this.env, ["CF_BUILD_ID", "CF_BUILD_URL"])
   }
 
   get isPR(): boolean {
-    return ensureEnvKeysExist(this.env, ["CF_PULL_REQUEST_NUMBER"])
+    return (
+      ensureEnvKeysExist(this.env, ["CF_PULL_REQUEST_NUMBER"]) &&
+      ensureEnvKeysAreInt(this.env, ["CF_PULL_REQUEST_NUMBER"])
+    )
   }
 
   get pullRequestID(): string {
@@ -32,13 +63,7 @@ export class Codefresh implements CISource {
     return owner && reponame ? `${owner}/${reponame}` : ""
   }
 
-  get repoURL(): string {
-    const owner = this.env.CF_REPO_OWNER
-    const reponame = this.env.CF_REPO_NAME
-    return owner && reponame ? `https://github.com/${owner}/${this.env.reponame}` : ""
-  }
-
   get ciRunURL() {
-    return this.env["CF_BUILD_URL"]
+    return this.env.CF_BUILD_URL
   }
 }
