8b429877e28231a1519a645fc2e3940217280cd9
Steven Peterson <steven.peterson@gmail.com>
2019-06-14 11:16:09 -0700

    Use commit URL to calculate the repo slug
    When building a fork, `CF_REPO_OWNER` refers to the forked repo instead of the target repo. The only environment variable currently available to derive the correct slug is `CF_COMMIT_URL`.

--- source/ci_source/providers/_tests/_codefresh.test.ts
+++ source/ci_source/providers/_tests/_codefresh.test.ts
@@ -2,8 +2,7 @@ import { Codefresh } from "../Codefresh"
 import { getCISourceForEnv } from "../../get_ci_source"
 
 const correctEnv = {
-  CF_REPO_OWNER: "codefresh",
-  CF_REPO_NAME: "someproject",
+  CF_COMMIT_URL: "https://github.com/codefresh/someproject/commit/123456",
   CF_BUILD_ID: "1501",
   CF_PULL_REQUEST_NUMBER: "800",
   CF_BUILD_URL: "https://g.codefresh.io/build/1234",
@@ -39,11 +38,10 @@ describe(".isPR", () => {
     expect(codefresh.isPR).toBeFalsy()
   })
 
-  const envs = ["CF_REPO_OWNER", "CF_REPO_NAME", "CF_BUILD_ID", "CF_PULL_REQUEST_NUMBER", "CF_BUILD_URL"]
+  const envs = ["CF_COMMIT_URL", "CF_BUILD_ID", "CF_PULL_REQUEST_NUMBER", "CF_BUILD_URL"]
   envs.forEach((key: string) => {
     let env = {
-      CF_REPO_OWNER: "codefresh",
-      CF_REPO_NAME: "someproject",
+      CF_COMMIT_URL: "https://github.com/codefresh/someproject/commit/123456",
       CF_BUILD_ID: "1501",
       CF_PULL_REQUEST_NUMBER: "800",
       CF_BUILD_URL: "https://g.codefresh.io/build/1234",
