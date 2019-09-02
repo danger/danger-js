d346570cddf6209e3acf1fd917657a37aa122aa9
kristof0425 <dombi.kristof@gmail.com>
2019-09-02 22:39:17 +0200

    docs(BuddyWorks): add CI setup comment

--- source/ci_source/providers/BuddyWorks.ts
+++ source/ci_source/providers/BuddyWorks.ts
@@ -1,6 +1,19 @@
 import { Env, CISource } from "../ci_source"
 import { ensureEnvKeysExist, ensureEnvKeysAreInt } from "../ci_source_helpers"
-// TODO add Setup comments
+/**
+ * ### CI Setup
+ *
+ * To make Danger run:
+ *
+ * - Create a new pipeline named, DangerJS, which is triggered on every push
+ * - Add a NodeJS environment as an Action
+ * - Go into it, head over to the bash editor and type the following
+ *    - `yarn install && yarn danger ci`
+ *    - or your npm script
+ * - Set the `DANGER_GITHUB_API_TOKEN` at the Variables section
+ * - You're done 🎉
+ *
+ */
 export class BuddyWorks implements CISource {
   constructor(private readonly env: Env) {}
 
