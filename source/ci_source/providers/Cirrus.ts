da4f66d8be0ef7fa60a6b1231248d04304cb2290
Reece Dunham <me@rdil.rocks>
2019-11-11 15:23:30 -0500

    Add instructions

--- source/ci_source/providers/Cirrus.ts
+++ source/ci_source/providers/Cirrus.ts
@@ -1,6 +1,24 @@
 import { Env, CISource } from "../ci_source"
 import { ensureEnvKeysExist, ensureEnvKeysAreInt } from "../ci_source_helpers"
 
+/**
+ * ### CI Setup
+ *
+ *  You need to edit your `.cirrus.yml` to add a `script` like this:
+ *
+ *   ```yaml
+ *     danger_script:
+ *       - yarn danger ci
+ *   ```
+ *
+ *  ### Token Setup
+ *
+ *  You need to add the `DANGER_GITHUB_API_TOKEN` environment variable, to do this,
+ *  go to your repo's settings, by clicking the gear at `https://cirrus-ci.com/github/[user]/[repo]`.
+ *  Generate the encrypted value, and add it to your `env` block.
+ *
+ *  Once you have added it, trigger a build.
+ */
 export class Cirrus implements CISource {
   constructor(private readonly env: Env) {}
 
