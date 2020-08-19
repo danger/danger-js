1a3df668a984682048c1e6c44dca8009377da3cb
Orta <git@orta.io>
2020-08-19 05:30:08 -0400

    Release 10.3.1

--- source/danger.d.ts
+++ source/danger.d.ts
@@ -1427,12 +1427,16 @@ interface GitHubReviewers {
 
 // getPlatformReviewDSLRepresentation
 interface GitLabJSONDSL {
+  /** Info about the repo */
   metadata: RepoMetaData
+  /** Info about the merge request */
   mr: GitLabMR
+  /** All of the individual commits in the merge request */
   commits: GitLabMRCommit[]
 }
 
 // danger.gitlab
+/** The GitLab metadata for your MR */
 interface GitLabDSL extends GitLabJSONDSL {
   utils: {
     fileContents(path: string, repoSlug?: string, ref?: string): Promise<string>
@@ -1477,40 +1481,49 @@ interface GitLabUserProfile extends GitLabUser {
 }
 
 interface GitLabMRBase {
-  /**  */
+  /** The MR's id */
   id: number
 
-  /**  */
+  /** The unique ID for this MR */
   iid: number
 
-  /**  */
+  /** The project ID for this MR */
   project_id: number
 
-  /**  */
+  /** The given name of the MR */
   title: string
 
-  /**  */
+  /** The body text describing the MR */
   description: string
 
-  /**  */
+  /** The MR's current availability */
   state: "closed" | "open" | "locked" | "merged"
 
-  /**  */
+  /** When was the MR made */
   created_at: string
 
-  /**  */
+  /** When was the MR updated */
   updated_at: string
 
+  /** What branch is this MR being merged into */
   target_branch: string
+  /** What branch is this MR come from */
   source_branch: string
+
+  /** How many folks have given it an upvote */
   upvotes: number
+  /** How many folks have given it an downvote */
   downvotes: number
 
+  /** Who made it */
   author: GitLabUser
+  /** Access rights for the user who created the MR */
   user: {
+    /** Does the author have access to merge? */
     can_merge: boolean
   }
-  assignee: GitLabUser
+  /** Who was assigned as the person to review */
+  assignee?: GitLabUser
   source_project_id: number
   target_project_id: number
   labels: string[]
@@ -1548,6 +1561,7 @@ interface GitLabMRBase {
   }
 }
 
+/** TODO: These need more comments from someone who uses GitLab, see GitLabDSL.ts in the danger-js repo */
 interface GitLabMR extends GitLabMRBase {
   squash: boolean
   subscribed: boolean
