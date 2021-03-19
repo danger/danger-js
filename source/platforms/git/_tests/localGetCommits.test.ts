5fbc0aa7428298a7fa496480f0a7f85e00f2744e
Daniel Ostapenko <daniel.ostapenko@toptal.com>
2021-03-19 12:33:21 +0200

    Fix unit test

--- source/platforms/git/_tests/localGetCommits.test.ts
+++ source/platforms/git/_tests/localGetCommits.test.ts
@@ -2,7 +2,7 @@ import { formatJSON } from "../localGetCommits"
 
 it("generates a JSON-like commit message", () => {
   expect(formatJSON).toEqual(
-    '{ "sha": "%H", "parents": "%p", "author": {"name": "%an", "email": "%ae", "date": "%ai" }, "committer": {"name": "%cn", "email": "%ce", "date": "%ci" }, "message": "%f"},'
+    '{ "sha": "%H", "parents": "%p", "author": {"name": "%an", "email": "%ae", "date": "%ai" }, "committer": {"name": "%cn", "email": "%ce", "date": "%ci" }, "message": "%s"},'
   )
 
   const withoutComma = formatJSON.substring(0, formatJSON.length - 1)
