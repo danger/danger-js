e3c4da343fd22fe0897ded5ba2ecf1ef480dd312
Frederic Barthelemy <git@fbartho.com>
2022-01-07 13:22:48 -0800

    Cleanup Typos

--- source/platforms/git/localGetHeadSHA.ts
+++ source/platforms/git/localGetHeadSHA.ts
@@ -5,12 +5,12 @@ const d = debug("localGetHeadSHA")
 
 export const localGetHeadSHA = () =>
   new Promise<string>(done => {
-    const call = `git rev-parse HEAD"`
+    const call = `git rev-parse HEAD`
     d(call)
 
     exec(call, (err, stdout, _stderr) => {
       if (err) {
-        console.error(`Could not get the git HEAD for the current path]`)
+        console.error(`Could not get the git HEAD for the current path`)
         console.error(err)
         return
       }
