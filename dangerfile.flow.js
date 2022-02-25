e89356a28205b5eb21cd3fc2b68dca324d48bf1e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-26 01:52:38 +0800

    Delete dangerfile.flow.js

--- dangerfile.flow.js
+++
@@ -1,8 +0,0 @@
-// This file isn't used anywhere, but it is used as a part of `yarn flow check`
-// to validate that flow typings work correctly for JS Dangerfiles
-
-// @flow
-
-import { danger } from "danger"
-
-danger.github.pr
