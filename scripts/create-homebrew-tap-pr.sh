40bdfe15ac41f7782478a780da48b3e436d65936
pepix <pepix2112@gmail.com>
2022-12-24 00:00:31 +0900

    Add a new workflow to build binaries for macOS architectres on GitHub Actions

--- scripts/create-homebrew-tap-pr.sh
+++ scripts/create-homebrew-tap-pr.sh
@@ -19,13 +19,21 @@ echo "SHA_X64=$SHA_X64"
 SHA_ARM64=$(shasum -a 256 ${FILE_ARM64} | cut -f 1 -d " ")
 echo "SHA_ARM64=$SHA_ARM64"
 
+# Set up SSH
+mkdir -p ~/.ssh
+echo "${HOMEBREW_TAP_DEPLOY_SECRET_KEY}" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+git config --global user.name danger
+git config --global user.email danger@users.noreply.github.com
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+ssh -o StrictHostKeyChecking=no -F /dev/null -vT git@github.com
+
 # Clone tap repo
 HOMEBREW_TAP_TMPDIR=$(mktemp -d)
-git clone --depth 1 https://github.com/danger/homebrew-tap.git "$HOMEBREW_TAP_TMPDIR"
+git clone --depth 1 git@github.com:danger/homebrew-tap.git "$HOMEBREW_TAP_TMPDIR"
 cd "$HOMEBREW_TAP_TMPDIR" || exit 1
-
-# git config user.name danger
-# git config user.email danger@users.noreply.github.com
 
 # Write formula
 echo "class DangerJs < Formula" > danger-js.rb
@@ -53,4 +61,6 @@ echo "end" >> danger-js.rb
 # Commit changes
 git add danger-js.rb
 git commit -m "Releasing danger-js version ${VERSION}"
+git remote rm origin
+git remote add origin git@github.com:danger/homebrew-tap.git
 git push origin master
