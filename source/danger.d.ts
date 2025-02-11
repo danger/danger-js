1cdf42842dd643b582192ccb73e9ad0ec30d3227
Orta Therox <git@orta.io>
2025-02-11 15:53:36 +0000

    Release 12.3.4

--- source/danger.d.ts
+++ source/danger.d.ts
@@ -1632,20 +1632,20 @@ declare const peril: PerilDSL
  * are wanting to introspect on whether a build has already failed.
  */
 declare const results: DangerRuntimeContainer
-type Pattern = string
-type Path = string
-type KeyedPatterns<T> = {
+export declare type Pattern = string
+export declare type Path = string
+export declare type KeyedPatterns<T> = {
   readonly [K in keyof T]: Pattern[]
 }
-type KeyedPaths<T> = {
+export declare type KeyedPaths<T> = {
   readonly [K in keyof T]: Path[]
 }
-type _MatchResult<T> = {
+export declare type _MatchResult<T> = {
   readonly [K in keyof T]: boolean
 }
-type MatchResult<T> = _MatchResult<T> & {
+export declare type MatchResult<T> = _MatchResult<T> & {
   /** Returns an object containing arrays of matched files instead of the usual boolean values. */
   getKeyedPaths(): KeyedPaths<T>
 }
 /** A vendored copy of the  Chainsmoker module on NPM */
-type Chainsmoker<T> = (...patterns: Pattern[]) => MatchResult<T>
+export declare type Chainsmoker<T> = (...patterns: Pattern[]) => MatchResult<T>
